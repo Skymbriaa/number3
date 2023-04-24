dabdb616b86efe35ff475ba2f83fa18f416bcb6e
Skymbriaa <pozilaaskymbria@gmail.com>
2023-04-24 19:37:46 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+
+{
+
+    static void Main(string[] args)
+
+    {
+
+        int number = 1000; // Початкове число
+
+        while (number < 10000) // Перевіряємо всі чотиризначні числа
+
+        {
+
+            if (number % 133 == 125 && number % 134 == 111)
+
+            {
+
+                Console.WriteLine(number); // Виводимо число, яке задовольняє умови
+
+            }
+
+            number++;
+
+        }
+
+    }
+
+}
